974d0621fea641373962fb7462e1804ce03ff847
githbkrishna <krishna636471@gmail.com>
2025-01-22 21:54:49 +0530

    style changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import '../app/main.css'
+'use client';
+
+import React from 'react';
+import '../app/main.css';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+  const router = useRouter();
+  
   return (
     <header>
-        <nav>
-            <div className="logo">
-                <div className="logo-icon"></div>
-                {/* <img src="/assets/logoo.png" alt=""> */}
-                TABVIEWER
-            </div>
-            <div className="nav-links">
-                <a href="#about">About</a>
-                <a href="#letsTry">Features</a>
-                <a href="#support">Support</a>
-            </div>
-            <button className="letsTry">Let's Try!</button>
-        </nav>
+      <nav>
+        <div className="logo">
+          <div className="logo-icon"></div>
+          TABVIEWER
+        </div>
+        <div className="nav-links">
+          <a href="#about" onClick={() => router.push(Footer)}>About</a>
+          <a href="#letsTry">Features</a>
+          <a href="#support">Support</a>
+        </div>
+        <button className="letsTry">Let's Try!</button>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
