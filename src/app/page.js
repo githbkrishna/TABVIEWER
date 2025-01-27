d3d697bdf8bfeeacbab0cab106fcedba4cf4a382
Hariom Singh <139392000+githbkrishna@users.noreply.github.com>
2025-01-28 00:09:46 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Preview from "@/components/Preview";
 import Stats from "@/components/Stats";
 import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
